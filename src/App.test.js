7763a15456922e1b070b40ddebe6f1cb6f69e48e
Julie <j49411@gmail.com>
2020-02-27 20:10:15 +0000

    refactor: add beforeEach function to app.test

--- src/App.test.js
+++ src/App.test.js
@@ -3,20 +3,23 @@ import { shallow } from 'enzyme';
 import CatApp from './App';
 
 describe('CatApp', () => {
+    
+    let wrapper;
   
+    beforeEach(() => {
+    wrapper = shallow(<CatApp />);
+  })
+
   it('shows the title', () => {
-    const wrapper = shallow(<CatApp />);
     const text = wrapper.find('p').text();
     expect(text).toEqual("Let's learn about cats!");
   });
 
   it('has a button to choose a breed', () => {
-    const wrapper = shallow(<CatApp />);
     expect(wrapper.find('button')).toHaveLength(1);
   });
 
   it('runs showBreeds function when clicked on the button', () => {
-    const wrapper = shallow(<CatApp />);
     const component = wrapper.instance();
     const showBreeds = jest
       .spyOn(component, 'showBreeds')
