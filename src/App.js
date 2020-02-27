96b0915d31bf4166deed450ccb5dc950353e3849
Julie <j49411@gmail.com>
2020-02-27 20:04:40 +0000

    feat: add a button that displays breeds

--- src/App.js
+++ src/App.js
@@ -1,13 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <p>Hello cat</p>
-    </div>
-  );
+class CatApp extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      showBreeds: false,
+      breedsData:[],
+      isLoading: false,
+    };
+    
+    this.showBreeds = this.showBreeds.bind(this);
+    this.closeBreeds = this.closeBreeds.bind(this);
+  }
+  
+  showBreeds() {
+
+    this.setState({ showBreeds: true }, () => {
+      document.addEventListener('click', this.closeBreeds);
+    });
+  }
+  
+  closeBreeds() {
+    this.setState({ showBreeds: false }, () => {
+      document.removeEventListener('click', this.closeBreeds);
+    });
+  }
+
+  componentDidMount() {
+    
+    fetch('https://api.thecatapi.com/v1/breeds?', {
+      headers: {'x-api-key': 'd3156788-df1b-4304-b4bf-6125607d2140'}}
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          isLoading: true,
+          breedsData: responseJson
+        })
+      })
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <div className="App">
+        <p>Let's learn about cats!</p>
+        </div>
+        <div className="Dropdown">
+          <button id='breedsButton' onClick={this.showBreeds}>
+            Breeds
+          </button>
+          
+          {this.state.showBreeds
+              ? (
+                <div className="Breeds">
+                    {this.state.breedsData.map((breed, key) => (
+                      <button key={key}>
+                        {breed.name}
+                    </button>
+                    ))}
+                </div>
+              )
+              : (
+                null
+              )
+          }
+        </div>
+      </div>
+    );
+  }
+  
 }
 
-export default App;
+export default CatApp;
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import CatApp from './App';
 
-describe('App component', () => {
-  it('starts with a count of 0', () => {
-    const wrapper = shallow(<App />);
+describe('CatApp', () => {
+  
+  it('shows the title', () => {
+    const wrapper = shallow(<CatApp />);
     const text = wrapper.find('p').text();
-    expect(text).toEqual('Hello cat');
+    expect(text).toEqual("Let's learn about cats!");
   });
+
+  it('has a button to choose a breed', () => {
+    const wrapper = shallow(<CatApp />);
+    expect(wrapper.find('button')).toHaveLength(1);
+  });
+
+  it('runs showBreeds function when clicked on the button', () => {
+    const wrapper = shallow(<CatApp />);
+    const component = wrapper.instance();
+    const showBreeds = jest
+      .spyOn(component, 'showBreeds')
+      .mockImplementation(() => Promise.resolve());
+      
+      component.forceUpdate();
+      wrapper.find('#breedsButton').simulate('click')
+      
+      expect(showBreeds).toHaveBeenCalled();
+  });
+
+  
+
 });
+
